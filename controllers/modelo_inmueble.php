4e500c34212f78cce5c0a7b1cdec65914d07eb86
sebastian <me@sebastiancastiblanco.com>
2019-08-12 16:36:22 -0500

    implementar api2

--- controllers/modelo_inmueble.php
+++ controllers/modelo_inmueble.php
@@ -12,13 +12,30 @@
 
             // a la variable "api" le asignamos el array con la iteraccion
             $api = $r[$i];
-            
+
             // Renombrar variables
             $administracion = $api['Administracion'];
             $alcobas = $api['Alcobas'];
         }
     }
+
     
+    
+    function modelo_listar_inmueble($r, $cantidad_inmuebles){
+        // Recorrer el array la cantidad de veces que se le indique
+        for ($i = 0; $i < $cantidad_inmuebles; $i++) {
+
+            // validar si existe la imagen, si no existe colocar la imagen de no imagen
+            $imagen = existeImagen(($r[$i]['foto1']));
+
+            // Eliminar el id de la inmobiliaria
+            $codigo = str_ireplace("813-", "", $r[$i]['Codigo_Inmueble']);
+
+            // a la variable "api" le asignamos el array con la iteraccion
+            $api = $r[$i];
+        }
+    }
+
     function modelo_inmueble_similares($r, $url){
         for ($i = 0; $i < count($r); $i++) {
             $imagen = existeImagen(($r[$i]['foto1']), $url);
@@ -26,6 +43,7 @@
             $api = $r[$i];
         }
     }
+
 
     function existeImagen($r) {
             if ($r == "") {
