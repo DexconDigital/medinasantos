f838a15b984ee178849a3cf74fdbfc690ff4fa99
sebas07casti <sebas07casti@gmail.com>
2019-08-14 10:30:51 -0500

    Finalizando Quienes somos

--- quienes-somos.php
+++ quienes-somos.php
@@ -19,19 +19,48 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-6">
-                            <div class="properties-service-v">
-
+                            <div class="properties-service-v quienes-somos">
+                                <img src="images/quienes_somos.jpg" alt="">
                             </div>
                         </div>
                         <div class="col-lg-6 align-self-center">
                             <div class="about-text">
-                                <h3>Welcome to RealEstast</h3>
-                                <p> <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit. ligula convallis, vel tincidunt ipsum posuere</b></p>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor arcu non ligula convallis, vel tincidunt
-                                    ipsum posuere. Fusce sodales lacus ut pellentes sollicitudin.Duis iaculis, arcu ut hendrerit pharetra, elit augue pulvinar
-                                    magna, a consectetur.Lorem ipsum dolor sit amet, consectetur.</p>
+                                <h3><?php echo $texto_quienes_somos['proyeccion']['titulo'] ?></h3>
+                                <p> <b><?php echo $texto_quienes_somos['proyeccion']['subtitulo'] ?></b></p>
+                                <?php for ($i = 0; $i < count($texto_quienes_somos['proyeccion']['parrafos']); $i++) {
+                                    echo '<p>' . $texto_quienes_somos['proyeccion']['parrafos'][$i] . '<p>';
+                                } ?>
                             </div>
                         </div>
+                    </div>
+                    <!-- Mision -->
+                    <div class="row">
+                        <div class="col-lg-6 align-self-center">
+                            <div class="about-text">
+                                <h3><?php echo $texto_quienes_somos['mision']['titulo'] ?></h3>
+                                <?php echo '<p>' . $texto_quienes_somos['mision']['parrafo'] . '<p>'; ?>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="properties-service-v mision">
+                                <img src="images/mision.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- vision -->
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="properties-service-v mision">
+                                <img src="images/vision.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-lg-6 align-self-center">
+                            <div class="about-text">
+                                <h3><?php echo $texto_quienes_somos['vision']['titulo'] ?></h3>
+                                <?php echo '<p>' . $texto_quienes_somos['vision']['parrafo'] . '<p>'; ?>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -43,30 +72,29 @@
                     <div class="row">
                         <div class="col-lg-3 col-md-3 col-sm-6">
                             <div class="counter-box">
-                                <i class="flaticon-tag"></i>
                                 <h1 class="counter Starting">500</h1>
-                                <h5>Lines of Sale</h5>
+                                <h5>Años de Experiencia</h5>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-6">
                             <div class="counter-box">
                                 <i class="flaticon-badge"></i>
                                 <h1 class="counter Starting">254</h1>
-                                <h5>Listings For Rent</h5>
+                                <h5>Total Inmuebles Disponibles</h5>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-6">
                             <div class="counter-box">
                                 <i class="flaticon-call-center-agent"></i>
                                 <h1 class="counter Starting">510</h1>
-                                <h5>Agents</h5>
+                                <h5>Inmuebles en Arriendo</h5>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-6">
                             <div class="counter-box">
                                 <i class="flaticon-job"></i>
                                 <h1 class="counter Starting">94</h1>
-                                <h5>Brokers</h5>
+                                <h5>Inmuebles en Venta</h5>
                             </div>
                         </div>
                     </div>
---
+++ servicios.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio</title>
+    <?php include 'layout/archivosheader.php'; ?>
+</head>
+
+<body>
+    <!-- Menu -->
+    <?php include 'layout/menu.php'; ?>
+    <!-- Fin de Menu -->
+    <div class="container-fluid body">
+  
+    </div>
+    <!-- Banner -->
+
+
+    <?php include 'layout/footer.php'; ?>
+    <script>
+        var pagina = false;
+    </script>
+    <?php include 'layout/archivosfooter.php'; ?>
+</body>
+
+</html>
