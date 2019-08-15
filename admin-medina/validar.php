f7c5c987441352d004c16d6e6815859518abcfb7
sebastian <me@sebastiancastiblanco.com>
2019-08-15 11:14:33 -0500

    modulo administrador

--- admin-medina/validar.php
+++ admin-medina/validar.php
@@ -7,9 +7,9 @@ $con=conect();
 
 $nombre=$_REQUEST["user"];
 $password=$_REQUEST["pass"];
-$id_inmobiliria = 3;
+$id_inmobiliaria = 6;
 
-$qry="SELECT usuario FROM usuarios WHERE usuario ='$nombre' and password ='$password' and id_inmobiliaria1 = '$id_inmobiliria'";
+$qry="SELECT usuario FROM usuarios WHERE usuario ='$nombre' and password ='$password' and id_inmobiliaria1 = '$id_inmobiliaria'";
 $sql=mysqli_query($con,$qry);
 
 $row = mysqli_fetch_row($sql);
@@ -19,7 +19,7 @@ if (mysqli_num_rows($sql) > 0) {
     session_start(); 
       //Guardamos dos variables de sesión que nos auxiliará para saber si se está o no "logueado" un usuario 
       $_SESSION["autentica"] = "SIP"; 
-      $_SESSION["usuarioactual"] =  $row[0]; 
+      $_SESSION["usuarioactual"] =  $row[0];
      header ("Location: admin/index.php");
       //nombre del usuario logueado. 
       //Direccionamos a nuestra página principal del sistema. 
