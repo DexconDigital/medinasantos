f97ec3819453451b94e842dfb2197aa8c7531db5
sebastian <me@sebastiancastiblanco.com>
2019-09-06 06:55:54 -0500

    actualizacion de las bases de datoos

--- admin-medina/admin/guardar-imagen.php
+++ admin-medina/admin/guardar-imagen.php
@@ -13,6 +13,6 @@ $id_inmo = 6;
 $con = Conect();
     copy($ruta,$destino);
 
-        mysqli_query($con, "INSERT INTO `slide_inicio` (`id`, `ruta_imagen`, `alt_imagen`, `texto-banner`, `id_inmobiliaria_5`) VALUES (NULL, '$destino', '$nombre', '$frase', '$id_inmo')");
+        mysqli_query($con, "INSERT INTO `slide_inicio` (`id`, `ruta_imagen`, `alt_imagen`, `texto-banner`, `id_inmobiliaria5`) VALUES (NULL, '$destino', '$nombre', '$frase', '$id_inmo')");
         header("Location: lista-imagenes.php");
 ?>
--- bases_de_datos/indexDB.php
+++ bases_de_datos/indexDB.php
@@ -3,5 +3,5 @@ require 'conexion.php';
 
 // Consulta Imagenes
 $con = Conect();
-$qry = "SELECT * FROM slide_inicio";
+$qry = "SELECT * FROM `slide_inicio` where `id_inmobiliaria5`= 6";
 $sql = mysqli_query($con, $qry);
