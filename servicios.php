eed59f97ee0ec017cc9b7c9dedd2c713189bd8e4
sebastian <me@sebastiancastiblanco.com>
2019-09-12 14:47:49 -0500

    quitar cuadros de servicios

--- servicios.php
+++ servicios.php
@@ -23,41 +23,41 @@
                     </div>
                     <div class="row">
                         <!-- item -->
-                        <div class="col-lg-4 col-md-6 col-sm-6 services-info-3 s-brd-2 wow fadeInLeft delay-04s" style="visibility: visible; animation-name: fadeInLeft;">
+                        <div class=" col-md-6 col-sm-6 services-info-3 s-brd-2 wow fadeInLeft delay-04s" style="visibility: visible; animation-name: fadeInLeft;">
                             <i class="<?php echo $iconos_servicios['arrendamientos'] ?>"></i>
                             <h5><?php echo $texto_servicios['arrendamientos']['titulo'] ?></h5>
 							<p style="text-align: justify"><?php echo $texto_servicios['arrendamientos']['parrafos'] ?></p>
                         </div>
                         <!-- item -->
-                        <div class="col-lg-4 col-md-6 col-sm-6 services-info-3 s-brd-1 wow fadeInUp delay-04s" style="visibility: visible; animation-name: fadeInUp;">
+                        <div class=" col-md-6 col-sm-6 services-info-3 s-brd-1 wow fadeInUp delay-04s" style="visibility: visible; animation-name: fadeInUp;">
                             <i class="<?php echo $iconos_servicios['ventas'] ?>"></i>
                             <h5><?php echo $texto_servicios['ventas']['titulo'] ?></h5>
 							<p style="text-align: justify"><?php echo $texto_servicios['ventas']['parrafos'] ?></p>
                         </div>
                         <!-- item -->
-                        <div class="col-lg-4 col-md-6 col-sm-6 services-info-3 wow fadeInRight delay-04s" style="visibility: visible; animation-name: fadeInRight;">
+                        <div class=" col-md-6 col-sm-6 services-info-3 wow fadeInRight delay-04s" style="visibility: visible; animation-name: fadeInRight;">
                             <i class="<?php echo $iconos_servicios['avaluos'] ?>"></i>
                             <h5><?php echo $texto_servicios['avaluos']['titulo'] ?></h5>
 							<p style="text-align: justify"><?php echo $texto_servicios['avaluos']['parrafos'] ?></p>
                         </div>
                         <!-- item -->
-                        <div class="col-lg-4 col-md-6 col-sm-6 services-info-3 s-brd-2 wow fadeInLeft delay-04s" style="visibility: visible; animation-name: fadeInLeft;">
+                        <div class=" col-md-6 col-sm-6 services-info-3 s-brd-2 wow fadeInLeft delay-04s" style="visibility: visible; animation-name: fadeInLeft;">
                             <i class="<?php echo $iconos_servicios['servicios_locativos'] ?>"></i>
                             <h5><?php echo $texto_servicios['servicios_locativos']['titulo'] ?></h5>
 							<p style="text-align: justify"><?php echo $texto_servicios['servicios_locativos']['parrafos'] ?></p>
                         </div>
-                        <!-- item -->
+                        <!-- item 
                         <div class="col-lg-4 col-md-6 col-sm-6 services-info-3 s-brd-1 wow fadeInDown delay-04s" style="visibility: visible; animation-name: fadeInDown;">
                             <i class="<?php echo $iconos_servicios['creditos_hipotecarios'] ?>"></i>
                             <h5><?php echo $texto_servicios['creditos_hipotecarios']['titulo'] ?></h5>
 							<p style="text-align: justify"><?php echo $texto_servicios['creditos_hipotecarios']['parrafos'] ?></p>
                         </div>
-                        <!-- item -->
+                        
                         <div class="col-lg-4 col-md-6 col-sm-6 services-info-3 wow fadeInRight delay-04s" style="visibility: visible; animation-name: fadeInRight;">
                             <i class="<?php echo $iconos_servicios['seguros'] ?>"></i>
                             <h5><?php echo $texto_servicios['seguros']['titulo'] ?></h5>
 							<p style="text-align: justify"><?php echo $texto_servicios['seguros']['parrafos'] ?></p>
-                        </div>
+                        </div>-->
                     </div>
                 </div>
             </div>
